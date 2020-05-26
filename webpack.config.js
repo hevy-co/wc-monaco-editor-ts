c3b905ae1f7d9a88638bb61d76513613d7fab2bd
Matt Olenik <olenikm@gmail.com>
2020-05-26 03:29:41 -0700

    Move types

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 
+// Work in somehow: npx tsc -d --declarationDir types --declarationMap --emitDeclarationOnly
 module.exports = {
     mode: "development",
     entry: {
